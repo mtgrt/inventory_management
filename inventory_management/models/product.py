18857e69131be0e2b5ef339ebde221d92c1e532c
mtgrt <mtgrt1369@gmail.com>
2024-06-11 23:20:57 +0300

    Update product.py

---
+++ inventory_management/models/product.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
+
+class Product(Base):
+    __tablename__ = 'products'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    quantity = Column(Integer)
+    location = Column(String)
+
+    supplier_id = Column(Integer, ForeignKey('suppliers.id'))
+    supplier = relationship("Supplier", back_populates="products")
